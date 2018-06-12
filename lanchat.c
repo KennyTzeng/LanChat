cce1af4ad8107bd039f2d1de58ab23c33f8f6289
Kenny Tseng <kenny29282000@gmail.com>
2018-06-12 08:18:03 +0800

    print network interface info done

--- lanchat.c
+++ lanchat.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
 #include <ifaddrs.h>
 #include <netdb.h>
 #include <linux/if_link.h>
-#include <linux/if.h>
+//#include <linux/if.h>
+#include <arpa/inet.h>
+#include <net/if.h>
 
 void getNetworkInterfaces();
 
@@ -21,59 +24,53 @@ int main(int argc, char *argv[]) {
 void getNetworkInterfaces() {
 
 	struct ifaddrs *ifaddr, *ifa;
-	int family, s, n;
-	char host[NI_MAXHOST];
 	struct ifreq ifr;
+	int interfaceIndex;
+	char addr[16], mask[16], broadaddr[16];
+
+	printf("Enumerated network interfaces:\n");
 
 	if(getifaddrs(&ifaddr) == -1) {
 		perror("getifaddrs");
 		exit(EXIT_FAILURE);
 	}
 
-	for(ifa = ifaddr, n = 0; ifa != NULL; ifa = ifa->ifa_next, n++) {
+	for(ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+		
 		if(ifa->ifa_addr == NULL) {
 			continue;
 		}
 
-		family = ifa->ifa_addr->sa_family;
+		if(ifa->ifa_addr->sa_family == AF_INET) {
+			
+			interfaceIndex = if_nametoindex(ifa->ifa_name);
+			strcpy(addr, inet_ntoa(((struct sockaddr_in *)ifa->ifa_addr)->sin_addr));
+			strcpy(mask, inet_ntoa(((struct sockaddr_in *)ifa->ifa_netmask)->sin_addr));
+			strcpy(broadaddr, inet_ntoa(((struct sockaddr_in *)((ifa->ifa_ifu).ifu_broadaddr))->sin_addr));			
+			printf("%-3d - %-10s %-15s %-15s (%-15s) ", interfaceIndex, ifa->ifa_name, addr, mask, broadaddr);
+			
 
-		printf("%-8s %s (%d)\n", 
-			ifa->ifa_name, 
-			(family == AF_PACKET)? "AF_PACKET" :
-			(family == AF_INET)? "AF_INET" :
-			(family == AF_INET6)? "AF_INET6" : "???",
-			family);
-
-		if(family == AF_INET || family == AF_INET6) {
-			s = getnameinfo(ifa->ifa_addr, (family == AF_INET)? sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6), host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
-			if(s != 0) {
-				printf("getnameinfo() failed: %s\n", gai_strerror(s));
-				exit(EXIT_FAILURE);
-			}
-
-			printf("\t\taddress: %s\n", host);
+			/*
+			printf("address: %s\n", inet_ntoa(((struct sockaddr_in *) ifa->ifa_addr)->sin_addr));
+			printf("mask: %s\n", inet_ntoa(((struct sockaddr_in *)ifa->ifa_netmask)->sin_addr));
+			printf("bmask: %s\n", inet_ntoa(((struct sockaddr_in *)((ifa->ifa_ifu).ifu_broadaddr))->sin_addr));			
+			*/
 
 			int fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
 			strcpy(ifr.ifr_name, ifa->ifa_name);
 			if(ioctl(fd, SIOCGIFHWADDR, &ifr) == 0) {
-				for(int i=0; i<6; i++) {
-					printf("%02x", (unsigned char) ifr.ifr_addr.sa_data[i]);
-				}
-				puts("\n");
+				printf("%02x:%02x:%02x:%02x:%02x:%02x\n", 
+					(unsigned char) ifr.ifr_addr.sa_data[0],
+					(unsigned char) ifr.ifr_addr.sa_data[1],
+					(unsigned char) ifr.ifr_addr.sa_data[2],
+					(unsigned char) ifr.ifr_addr.sa_data[3],
+					(unsigned char) ifr.ifr_addr.sa_data[4],
+					(unsigned char) ifr.ifr_addr.sa_data[5]);
 			}
 			close(fd);
-
-		}else if(family == AF_PACKET && ifa->ifa_data != NULL) {
-			struct rtnl_link_stats *stats = ifa->ifa_data;
-
-			printf("\t\ttx_packets = %10u; rx_packets = %10u\n"
-				   "\t\ttx_bytes   = %10u; rx_bytes   = %10u\n",
-				   stats->tx_packets, stats->rx_packets,
-				   stats->tx_bytes, stats->rx_bytes);
 			
-
-			printf("%s\n", ifa->ifa_data);
 		}
+
 	}
 
 	freeifaddrs(ifaddr);
